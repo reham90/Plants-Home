67a3cde3a4b681620043aabdd1243d3007a6095f
reham <reham344@gmail.com>
2025-03-27 23:41:53 +0300

    edit

---
+++ js/script.js
@@ -0,0 +1,507 @@
+$(window).on('load', function() {
+  $(".preloader").hide();
+});
+$(document).ready(function() {
+  new WOW().init();
+
+
+
+  //phone size menu onclick
+  if ($(window).width() <= 991) {
+
+      $(".menu-id").click(function(e) {
+          e.preventDefault();
+
+          $(".navgition").toggleClass("reset-left");
+
+          $("body").toggleClass("overflow");
+
+
+      });
+     
+      //slide down menu
+      $(".menu-item-has-children").click(function(e) {
+          e.preventDefault();
+          $(this).find(".sub-menu ").slideToggle(400);
+          $(".menu-item-has-children ").not(this).find(".sub-menu ").slideUp(400);
+          if ($(window).width() <= 991) {
+
+              $(this).toggleClass("active");
+              $(".menu-item-has-children ").not(this).removeClass("active");
+
+          }
+      });
+  }
+
+  
+
+  //fixed nav
+  $(window).on("scroll", function() {
+      var scroll = $(window).scrollTop();
+      var $stickyNav = $(".top-header");
+      var $stickyHeader = $("header");
+      var windowWidth = $(window).width();
+      // Handle fixed navigation
+      $stickyNav.toggleClass("fixed-nav", scroll >= 200);
+      // Handle fixed header
+      if (lastScroll - scroll > 0 || (lastScroll - scroll >= 0 && windowWidth <= 991)) {
+          $stickyHeader.addClass("fixed-header");
+          $('.search-content').removeClass('height-when-close', 1000);
+      }
+
+      if (scroll === 0) {
+          $stickyNav.removeClass("fixed-header");
+          $stickyHeader.removeClass("fixed-header");
+          $('.search-content').addClass('height-when-close', 500);
+          $('.search-content').removeClass('height-when-open', 500);
+          $('.search-btn').removeClass('close-search');
+      }
+      if (lastScroll - scroll < 0) {
+          $stickyNav.removeClass("fixed-header");
+          $stickyHeader.removeClass("fixed-header");
+      }
+      lastScroll = scroll;
+  });
+
+  var lastScroll = 0;
+
+
+
+  //////////** fixed arrow to top**//////////
+  $(".arrow-top").click(function() {
+      $("html,body").animate({
+              scrollTop: 0,
+          },
+          1000
+      );
+  });
+  $(this).scrollTop() >= 500 ?
+      $(".arrow-top").fadeIn(300) :
+      $(".arrow-top").fadeOut(300);
+
+  $(window).scroll(function() {
+      $(this).scrollTop() >= 500 ?
+          $(".arrow-top").fadeIn(300) :
+          $(".arrow-top").fadeOut(300);
+  });
+
+  ///////// ** main-slider ** /////////
+  var mainSlider = new Swiper(".main-slider .swiper-container", {
+      loop: true,
+      autoplay: true,
+      pagination: {
+          el: ".main-slider .swiper-pagination",
+          clickable: true,
+      },
+      navigation: {
+          nextEl: ".main-slider .swiper-btn-next",
+          prevEl: ".main-slider .swiper-btn-prev",
+      },
+      breakpoints: {
+          0: {
+              slidesPerView: 1,
+          },
+          767: {
+              slidesPerView: 1,
+          },
+          992: {
+              slidesPerView: 1,
+          },
+          1199: {
+              slidesPerView: 1,
+          },
+      },
+  });
+
+
+ 
+
+ 
+
+
+
+  
+
+  if ($(window).width() <= 991) {
+///////// **category-section** /////////
+
+var category = new Swiper(".category-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".category-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".category-slider .swiper-btn-next",
+        prevEl: ".category-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 6,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 7,
+            spaceBetween: 24,
+        },
+    },
+});
+
+///////// **specials-section** /////////
+
+var specials = new Swiper(".specials-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".specials-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".specials-slider .swiper-btn-next",
+        prevEl: ".specials-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
+
+
+///////// **small-plants-slider-section** /////////
+
+var smallPlants = new Swiper(".small-plants-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".small-plants-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".small-plants-slider .swiper-btn-next",
+        prevEl: ".small-plants-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
+
+///////// **big-plants-slider-section** /////////
+
+var bigPlants = new Swiper(".big-plants-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".big-plants-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".big-plants-slider .swiper-btn-next",
+        prevEl: ".big-plants-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
+
+
+///////// **care-plants-slider-section** /////////
+
+var carePlants = new Swiper(".care-plants-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".care-plants-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".care-plants-slider .swiper-btn-next",
+        prevEl: ".care-plants-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
+
+
+///////// **alovera-plants-slider-section** /////////
+
+var aloveraPlants = new Swiper(".alovera-plants-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".alovera-plants-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".alovera-plants-slider .swiper-btn-next",
+        prevEl: ".alovera-plants-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
+
+
+
+  }
+
+
+  ///////// **testimonials-slider-section** /////////
+
+var testimonials = new Swiper(".testimonials-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: ".testimonials-slider .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".testimonials-slider .swiper-btn-next",
+        prevEl: ".testimonials-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        720: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1199: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    },
+});
+
+  
+/////////////////////// count down ////////////////////
+
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "09/30/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            // document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
+
+
+    (function () {
+        const second = 1000,
+              minute = second * 60,
+              hour = minute * 60,
+              day = hour * 24;
+      
+        //I'm adding this section so I don't have to keep updating this pen every year :-)
+        //remove this if you don't need it
+        let today = new Date(),
+            dd = String(today.getDate()).padStart(2, "0"),
+            mm = String(today.getMonth() + 1).padStart(2, "0"),
+            yyyy = today.getFullYear(),
+            nextYear = yyyy + 1,
+            dayMonth = "09/30/",
+            birthday = dayMonth + yyyy;
+        
+        today = mm + "/" + dd + "/" + yyyy;
+        if (today > birthday) {
+          birthday = dayMonth + nextYear;
+        }
+        //end
+        
+        const countDown = new Date(birthday).getTime(),
+            x = setInterval(function() {    
+      
+              const now = new Date().getTime(),
+                    distance = countDown - now;
+      
+              document.getElementById("days2").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours2").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes2").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds2").innerText = Math.floor((distance % (minute)) / second);
+      
+              //do something later when date is reached
+              if (distance < 0) {
+                // document.getElementById("headline").innerText = "It's my birthday!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+              }
+              //seconds
+            }, 0)
+        }());
+  
+  ////////////** footer transfer into accordion **//////////
+
+  if ($(window).width() <= 991) {
+      $(".footer-accordion").click(function() {
+          var x = $(this).siblings().prop("scrollHeight") + 15 + "px";
+          $(".footer-accordion").not(this).removeClass("active");
+          $(this).toggleClass("active");
+          if ($(this).siblings().css("max-height") == "0px") {
+              $(this).siblings().css("max-height", x);
+              $(this).siblings(".nav-foot").css("padding-top", "15px");
+          } else {
+              $(this).siblings().css("max-height", "0");
+              $(this).siblings(".nav-foot").css("padding-top", "0");
+          }
+
+
+      });
+  };
+
+  $(".close-icon").click(function(){
+    $(".greeting-bar ").remove();
+  });
+});
